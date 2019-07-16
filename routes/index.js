a4558bc532ceebfc07bccb94cea38c0084ecf9c5
Henry Feng <henryfeng12345@gmail.com>
2019-07-16 14:36:52 -0500

    Move to Readme file

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,27 @@
 const express = require('express');
 const router = express.Router();
 
+// index.js uses es6 formatting 
+//The index.js will allow users to sign up or login
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', (req, res, next) => {
   res.render('index', { title: 'Surf Shop - Home' });
+});
+
+/* GET /register home page. */
+router.get('/register', (req, res, next) => {
+  res.send('GET /register');
+});
+
+/* POST /register home page. */
+router.post('/register', (req, res, next) => {
+  res.send('POST /register');
+});
+
+/* POST /login home page. */
+router.post('/login', (req, res, next) => {
+  res.send('GET /login');
 });
 
 module.exports = router;
