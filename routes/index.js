085b05e4b5f2464538f8ba453cce89d70e806e02
Henry Feng <henryfeng12345@gmail.com>
2019-07-16 14:55:21 -0500

    Add user and index routes

--- routes/index.js
+++ routes/index.js
@@ -24,4 +24,39 @@ router.post('/login', (req, res, next) => {
   res.send('GET /login');
 });
 
+//Displays user profile information
+/* GET /profile home page. */
+router.get('/profile', (req, res, next) => {
+  res.send('GET /profile');
+});
+
+//Update user profile information
+/* PUT /profile/:user_id home page. */
+router.put('/profile/:user_id', (req, res, next) => {
+  res.send('PUT /profile/:user_id');
+});
+
+/* GET /forgot-pw home page. */
+router.get('/forgot', (req, res, next) => {
+  res.send('GET /forgot');
+});
+
+// :id won't be necessary because the user would be typing in a 
+// unique email. We will use that email to look up that specific 
+// user. 
+/* PUT /forgot-pw home page. */
+router.put('/forgot', (req, res, next) => {
+  res.send('PUT /forgot');
+});
+
+/* GET /reset-pw home page. */
+router.get('/reset/:token', (req, res, next) => {
+  res.send('GET /reset/:token');
+});
+
+/* PUT /reset-pw home page. */
+router.put('/reset-pw/:token', (req, res, next) => {
+  res.send('PUT /reset/:token');
+});
+
 module.exports = router;
