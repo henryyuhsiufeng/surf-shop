4e1e0297dac07d4ceb058c0b6c651c1e60bdedc7
Henry Feng <henryfeng12345@gmail.com>
2019-07-17 10:05:44 -0500

    update controllers

--- controllers/index.js
+++ controllers/index.js
@@ -5,10 +5,15 @@ module.exports = {
     //create a method that we can use with post register route in user index
     //this is a method of this parent object
     postRegister(req, res, next) {
+        const newUser = new User({
+            username: req.body.username,
+            email: req.body.email,
+            image: req.body.image
+        })
         //user register with a new username and we are pulling the username from the form
         //the we pass the password as the second parameter, which then takes the password
         //hashes and salts it.
-        User.register(new User({username: req.body.username}), req.body.password, (err) => {
+        User.register(newUser, req.body.password, (err) => {
           if (err) {
             console.log('error while user register!', err);
             return next(err);
