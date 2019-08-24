24d5e4336d805e15a7788e3a26c7526ce3d4e898
Henry Feng <henryfeng12345@gmail.com>
2019-08-24 10:37:48 -0500

    back to work

--- routes/posts.js
+++ routes/posts.js
@@ -39,14 +39,16 @@ router.post('/', isLoggedIn ,upload.array('images', 4), asyncErrorHandler(postCr
 /* GET posts show /posts/:id */
 router.get('/:id', asyncErrorHandler(postShow));
 
+
+//isAuthor requires that a user isLoggedIn
 /* GET posts edit /posts/:id/edit */
-router.get('/:id/edit',asyncErrorHandler(isAuthor), asyncErrorHandler(postEdit));
+router.get('/:id/edit', isLoggedIn, asyncErrorHandler(isAuthor), asyncErrorHandler(postEdit));
 
 /* PUT posts update /posts/:id */
-router.put('/:id', asyncErrorHandler(isAuthor), upload.array('images', 4), asyncErrorHandler(postUpdate));
+router.put('/:id', isLoggedIn, asyncErrorHandler(isAuthor), upload.array('images', 4), asyncErrorHandler(postUpdate));
 
 /* DELETE posts destroy /posts/:id */
-router.delete('/:id', asyncErrorHandler(isAuthor), asyncErrorHandler(postDestroy));
+router.delete('/:id', isLoggedIn, asyncErrorHandler(isAuthor), asyncErrorHandler(postDestroy));
 
 
 module.exports = router;
